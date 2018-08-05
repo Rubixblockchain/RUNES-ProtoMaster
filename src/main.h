04843047bb2d3a69324b9c1c04e9948c823e0323
Rubix Dev <rubixblockchain@gmail.com>
2018-08-05 10:38:05 -0700

    yup webhhok test again
    added the `s` back in the comment

--- src/main.h
+++ src/main.h
@@ -2,7 +2,7 @@
 // Copyright (c) 2009-2014 The Bitcoin developers
 // Copyright (c) 2014-2015 The Dash developers
 // Copyright (c) 2016-2017 The Lux developers
-// Copyright (c) 2017-2018 The RÜNES developer
+// Copyright (c) 2017-2018 The RÜNES developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
